c13a7998bfdf7d7444a9429597d26030585b8db8
jalan <jalan@vmware.com>
2023-01-31 22:31:12 +0000

    Updated Projects page
    Signed-off-by: jalan <jalan@vmware.com>

--- src/data/info.ts
+++ src/data/info.ts
@@ -2,51 +2,21 @@ export const info = {
   baseUrl: "https://astro-portfolio-uzair.vercel.app",
   name: "Alan Jaison",
   jobDescription: "Web Developer",
-  about: `I'm a software engineer based in Southampton, United Kingdom.
-   I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.
-   I have been working for 3 year, with a demonstrated history of working in the Computer Software industry. Skilled in Front-end Development.
-   I have 3 years of experience in HTML, CSS, SCSS, JavaScript, Bootstrap, tailwind CSS, StyledComponent, React JS and Next JS. Hence,
-   I have successfully completed several web projects from scratch. 
-   Yet, I learn continuously and never stop exploring new technologies.
-   So, I have also gained knowledge of Node JS, Express JS, Mongo DB, Python,
-   firebase and React Native with time.
+  about: `Software Engineer with 1.5+ years of experience leading technological transformation through code and modern development practices. 
+
+I assist companies enable their engineering teams, modernise and build new applications using technologies such as Go, React, Spring boot, 
+Python, C++ and Kubernetes, RabbitMQ, Kafka whilst incorporating development principles such as CI/CD, XP, Agile and TDD.
   `,
 
   experience: [
     {
-      name: "InterNative Labs, LLC",
-      location: "Karāchi, Sindh, Pakistan",
-      startDate: "Apr 2022",
+      name: "VMware",
+      location: "EMEA Remote",
+      startDate: "June 2022",
       endDate: "Present",
       description: [
         "- Developing screens and UI components for the web application using React and Tailwind.",
         "- Fixing UI issues and integrating backend APIs with Redux Saga.",
-      ],
-    },
-
-    {
-      name: "VMware (Remote)",
-      location: "Amsterdam, North Holland, Netherlands",
-      startDate: "Nov 2020",
-      endDate: "Apr 2022",
-      description: [
-        "✓ Developing code in accordance with appropriate design and architectural rules",
-        "✓ Documenting, testing, deployment, maintenance, support of platforms, applications, databases and solutions",
-        "✓ Help and training of other colleagues",
-        "✓ Others tasks connected to the nature of the job",
-        "✓ Code Improvements and Refactoring ",
-        "✓ Best Practices and Rules Maintainer",
-      ],
-    },
-
-    {
-      name: "Interns Pakistan",
-      location: "Pakistan",
-      startDate: "Oct 2020",
-      endDate: "Nov 2020",
-      description: [
-        "An Immersive Internship Program that prepares me for the world's most in-demand career. A six weeks program completely based on learning and challenges.",
-        "The internship track is built by an industry expert that let develop the skills in the areas most important ",
       ],
     },
   ],
@@ -87,7 +57,7 @@ export const info = {
 
   projects: [
     {
-      title: "Astro Portfolio",
+      title: "Recruitment Website",
       isFeatured: true,
       thumbnail: "/assets/images/astro-portfolio.png",
       githubUrl: "https://github.com/uzzii-21/astro-portfolio",
